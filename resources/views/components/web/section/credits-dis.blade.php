c161fe21ef4cf5facd37e3cb0f91df8c893d2532
Perla <pataditaenloshuevos@gmail.com>
2022-07-01 20:40:09 -0500

    home and stories lists

---
+++ resources/views/components/web/section/credits-dis.blade.php
@@ -0,0 +1,5 @@
+<div class="inv">
+<p><b>DISEÑO GRÁFICO</b></p>
+<hr class="creditshr">
+<x-web.section.img-credits  />
+</div>
---
+++ resources/views/layouts/web/home-cocreation/content-citas.blade.php
@@ -0,0 +1,25 @@
+<div style="background-color:#f0efe6;">
+<section class="section-quotes-coc">
+<div class="quotes__title">
+    <img class='img-quotes'src="{{ asset('assets') }}/web/icons/home-coc-color.svg" alt="comillas" />
+    <br style="background-color:#f0efe6;">
+    <br style="background-color:#f0efe6;">
+    <h2 >Lorem ipsum dolor sit amet consectetur adipisicing elit..</h2>
+    </div>
+
+    <div class="section-quotes">
+        <p class="quotes__text ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, aut ab beatae, illum dicta ratione sit
+			voluptatum, ea optio dignissimos quae.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, aut ab beatae, illum dicta ratione sit
+			voluptatum, ea optio dignissimos quae.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, aut ab beatae, illum dicta ratione sit
+			voluptatum, ea optio dignissimos quae.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, aut ab beatae, illum dicta ratione sit
+			voluptatum, ea optio dignissimos quae.</p>
+
+    </div>
+</section>
+<br style="background-color:#f0efe6;">
+<br style="background-color:#f0efe6;">
+
+</div>
