0fab0ef128fc24b2d51d6bcd145eab9311102809
Perla <pataditaenloshuevos@gmail.com>
2022-07-07 12:56:16 -0500

    merge

--- resources/views/components/web/section/home-coc.blade.php
+++ resources/views/components/web/section/home-coc.blade.php
@@ -1,4 +1,4 @@
-<div class="wrapper-most-read">
+<div class="wrapper-coc">
 	<img src="https://cdn.zeplin.io/626c221a3745ab96ae1e4665/assets/82DCC51C-5A3B-488A-B61F-4898BDDE6C30.png"
 		alt="image">
 	<div class="text-most-read">
