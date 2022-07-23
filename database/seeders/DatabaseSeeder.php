4da99a2d24b9f4b68e1e6d183b94ff41c5bb89b2
Gerardo Carvajal <gcarvajal@robinfood.com>
2022-07-22 19:35:44 -0500

    ajuste email

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,21 +16,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserAdminitratorSeeder::class);
-        User::factory(10)
-            ->has(UserProfile::factory()->count(1))
-            ->create();
         \App\Models\Users\Term::factory(1)->create();
-        \App\Models\Utils\Region::factory(10)->create();
-        \App\Models\Utils\Tag::factory(10)->create();
-
         $this->call(RolesSeeder::class);
         $this->call(UserGenderSeeder::class);
         $this->call(UtilsSeeder::class);
         $this->call(SpecialRolesSeeder::class);
         $this->call(SpecialSeeder::class);
-        $this->call(SpecialTwoSeeder::class);
-        $this->call(SpecialThreeSeeder::class);
-
         $this->call(ArticleRolesSeeder::class);
         $this->call(ArticleSeeder::class);
     }
